249b02d22a0d99295e723a6c5839c20a0720fd40
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2024-04-03 14:54:13 +0000

    Main Commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+"use strict";
+// Script for Area of Trapezoid
+
+// Elements
+// Inputs
+const baseInput = document.getElementById('base1Input');
+const base2Input = document.getElementById('base2Input');
+const heightInput = document.getElementById('heightInput');
+// Result Display
+const areaResult = document.getElementById('AreaResult');
+// Calculate Button
+const calculateBtn = document.getElementById('CalculateBtn');
+
+// Code for calculating area
+function Calculate() {
+
+  // Get Inputs
+  let base1 = Number(base1Input.value);
+  let base2 = Number(base2Input.value);
+  let height = Number(heightInput.value);
+
+  // Calculate Area of Trapezoid using Formula
+  let area = (base1 + base2) * height * 0.5;
+
+  // Round up to 2 decimal places, if needed
+  if (area % 1 != 0) {
+    area = area.toFixed(2);
+  }
+
+  // Display Result
+  areaResult.innerHTML = "" + area + " m<sup>2</sup>";
+  return;
+}
+
+// Connect button click to the Calculate() function, 
+calculateBtn.onclick = Calculate;
+
+// Initial Calculate() call
+Calculate();
